6579e40c4c2bdeb4e8c8c588e1bb6acc24623e31
EnderDemon606 <wolflagerwey@gmail.com>
2026-08-26 12:36:21 +0200

    Push

---
+++ Assets/Scripst/BordScript.cs
@@ -0,0 +1,25 @@
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
+
+public class BordScript : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Vector2 displayOffset;
+    private Vector2 originalPos;
+
+    private void Start()
+    {
+        originalPos = transform.position;
+    }
+
+    public void OnPointerEnter(PointerEventData data)
+    {
+        transform.position = originalPos + displayOffset;
+    }
+
+    public void OnPointerExit(PointerEventData data)
+    {
+        transform.position = originalPos;
+    }
+}

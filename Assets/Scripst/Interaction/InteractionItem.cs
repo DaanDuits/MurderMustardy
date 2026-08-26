b813ffef112052226f7646e45d5e5fe2c1ddcd1a
Daan Duits <daand04@gmail.com>
2026-08-26 12:54:19 +0200

    Hover bahaviour

--- Assets/Scripst/Interaction/InteractionItem.cs
+++ Assets/Scripst/Interaction/InteractionItem.cs
@@ -1,8 +1,30 @@
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class InteractionItem : MonoBehaviour, IPointerDownHandler
+[RequireComponent(typeof(SpriteRenderer), typeof(BoxCollider2D))]
+public class InteractionItem : MonoBehaviour, IPointerDownHandler, IPointerEnterHandler, IPointerExitHandler
 {
+    [SerializeField] private Sprite hoverSprite;
+    private Sprite _defaultSprite;
+    private SpriteRenderer _spriteRenderer;
+
+    private void Awake()
+    {
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+        _defaultSprite = _spriteRenderer.sprite;
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        _spriteRenderer.sprite = hoverSprite;
+    }
+
+    public void OnPointerExit(PointerEventData eventData) 
+    {
+        _spriteRenderer.sprite = _defaultSprite;
+    }
+
     public void OnPointerDown(PointerEventData eventData)
     {
         Debug.Log("Click");
